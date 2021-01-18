ffb588802648fba82b4ae93b5ff99c83b7e033c1
Prime351585 <76157279+Prime351585@users.noreply.github.com>
2021-01-18 16:30:07 +0530

    Update Final.py
    Added Predicting the height of UAV for ~99% accuracy with plot

--- UAV/Final.py
+++ UAV/Final.py
@@ -1,18 +1,28 @@
+import os
 import math
 import numpy as np
 import matplotlib.pyplot as plt
 from math import radians, cos, sin, asin, sqrt
 # for graphing ///////////////////////////////////////////////////////////////////////////////////
-Y=[]
-X=[]
-T=[]
-# for reading given file data /////////////////////////////////////////////////////////////////////
-initialwaypoints=open(r"initial_waypoints.txt") #Enter the path of initialwaypoints.txt file
+Y=[]                             # Actual Y coordinate Array
+X=[]                             # Actual X coordinate Array
+T=[]                             # Time Array
+XPredFinal=[]                    # Predicted X coordinate Array
+YPredFinal=[]                    # Predicted Y coordinate Array
+Acc=[]                           # Accuracy Array
+
+# For reading given file data /////////////////////////////////////////////////////////////////////
+Workingdirectory=os.getcwd()
+filepath=Workingdirectory+'\initial_waypoints.txt'
+print(filepath)
+initialwaypoints=open(filepath) #Enter the path of initialwaypoints.txt file
+
 # initial Waypoints 
-latitude=[]
-longitude=[]
-VelocityX=[]
-VelocityY=[]
+latitude=[]                      # Latitude Array Readed from file 
+longitude=[]                     # Longitude Array Readed from file
+VelocityX=[]                     # Velocity in X direction as readed from file
+VelocityY=[]                     # Velocity in Y direction as readed from file
+
 for x in initialwaypoints:
     c=x.split(" ")
     latitude.append(c[0])
@@ -20,14 +30,17 @@ for x in initialwaypoints:
     VelocityX.append(c[2])
     VelocityY.append(c[3])
 initialwaypoints.close
+
 # Final Waypoints
-finalwaypoints=open(r"final_waypoints.txt")#Enter the path of finalwaypoints.txt file
+filepath=Workingdirectory+r"\final_waypoints.txt"
+finalwaypoints=open(filepath)#Enter the path of finalwaypoints.txt file
 for x in finalwaypoints:
     c=x.split(" ")
     latitude.append(c[0])
     longitude.append(c[1])
 FINALWAYPOINT=[latitude[-1],longitude[-1]]
 finalwaypoints.close
+
 # HAVERSINE FUNCTION ////////////////////////////////////////////////////////////////////////////////
 def haversine(lon1, lat1, lon2, lat2):
     """
@@ -43,70 +56,133 @@ def haversine(lon1, lat1, lon2, lat2):
     c = 2 * asin(sqrt(a)) 
     r = 6371000                  # Radius of earth in kilometers. Use 3956 for miles
     return c * r
-print('TESTING',latitude)
 # checking the waypoints////////////////////////////////////////////////////////////////////////////
-W=[]
+
+W=[]                             # Wyapoint List
 print('checking for Waypoints')
+
 for j in range(0,len(latitude)-1):
     W.append(haversine(float(longitude[j]),float(latitude[j]),float(longitude[-1]),float(latitude[-1])))
     print('Distance of final Waypoint from Waypoint',j+1,'=',W[j],'m')
+
 minimumdistance=min(W)           # Finding Minimum Distance
 pos=W.index(minimumdistance)     # Finding index of Minimum distance
 print('Minimum Distance Found is between Final Waypoint and Waypoint',pos+1,'= ',minimumdistance,'m')
+
 Ux=float(VelocityX[pos])         # x velocity of drone
 Uy=float(VelocityY[pos])         # y velocity of drone
+
 print('Velocity of UAV in x direction=',Ux)
 print('Velocity of UAV in y direction =',Uy)
 
-A=math.pi*0.05**2                # for radius = 5 cm    
+A=math.pi*0.05**2                # Surface Area for a Spherical payload of radius = 5 cm    
 P=1.225                          # density of air
 C=0.5                            # AIr drag corfficient
 M=1.5                            # Mass of payload
 Gravity=9.81                     # Gravity
-t=0
-Fdragx=0
-Xi=0
+t=0                              # initial time
+Fdragx=0                         # initial Drag Force
+Xi=0                             # initial X Position Of drone 
 Yi=22                            # Hieght of Drone can be calculated 
 Vw=2                             # Wind velocity can be altered from here
-# Resultant velocity ////////////////////////////////////////////////////////////////////
-theta=math.atan(Uy/Ux)#//////// velocity vector angle
+
+# Resultant velocity //////////////////////////////////////////////////////////////////////////////
+theta=math.atan(Uy/Ux)           # velocity vector angle
 U=sqrt((Ux)**(2)+(Uy)**2+2*math.cos(theta)*Ux*Uy)+Vw
-Verticalvel=0
+Verticalvel=0                    # velocity of UAV in y axis
 DelT=0.01                        # Small interval of time
-for t in np.arange(0,60,DelT):   # For 1 Minute
-    # drag force in horizontal direction
-    Fdragx=(-1/2)*(P*(U**2)*C*A)
-    # initial acceleration in horizontal direction
-    Aix=Fdragx/M
-    # velocity in horizontal direction
-    Vx=U+Aix*DelT
-    # distance in horizontal direction
-    Xf=Xi+U*DelT+(1/2)*Aix*((DelT)**2)
-    U=Vx
-    Xi=Xf
-    # drag force in vertical direction
-    Fdragy=(1/2)*(P*(Verticalvel**2)*C*A)
-    # initial acceleration in vertical direction
-    Aiy=(Fdragy/M)-Gravity
-    # velocity in horizontal direction
-    Vy=Verticalvel+Aiy*DelT
-    # distance in horizontal direction
-    Yf=Yi+Verticalvel*DelT+(1/2)*Aiy*((DelT)**2)
-    Verticalvel=Vy
-    Yi=Yf
-    # FOR GRAPH
-    X.append(Xf)
-    Y.append(Yf)
-    T.append(t)
-    Error=abs(((minimumdistance-Xf)/minimumdistance))
-    Accuracy=1-Error
-    if Yf<0:
-        print('Paylod dropped successfully','Paylod drop location',Xf,'m','Drop time',t,'sec','Accuracy =',Accuracy*100,'%')
+
+# Main Calculation function ///////////////////////////////////////////////////////////////////////
+
+def Trajectorycal(U,DelT,Xi,Yi,Verticalvel):
+    
+    for t in np.arange(0,60,DelT):   # Upto 1 Minute of dropping time,for More then 1 minute of dropping change 60 to desired time in seconds. 
+        
+        # drag force in horizontal direction
+        Fdragx=(-1/2)*(P*(U**2)*C*A)
+        
+        # initial acceleration in horizontal direction
+        Aix=Fdragx/M
+        
+        # velocity in horizontal direction
+        Vx=U+Aix*DelT
+        
+        # distance in horizontal direction
+        Xf=Xi+U*DelT+(1/2)*Aix*((DelT)**2)
+        U=Vx
+        Xi=Xf
+        
+        # drag force in vertical direction
+        Fdragy=(1/2)*(P*(Verticalvel**2)*C*A)
+        
+        # initial acceleration in vertical direction
+        Aiy=(Fdragy/M)-Gravity
+        
+        # velocity in horizontal direction
+        Vy=Verticalvel+Aiy*DelT
+        
+        # distance in horizontal direction
+        Yf=Yi+Verticalvel*DelT+(1/2)*Aiy*((DelT)**2)
+        Verticalvel=Vy
+        Yi=Yf
+        Error=abs(((minimumdistance-Xf)/minimumdistance))
+        Accuracy=1-Error
+        
+        # FOR FURTHER GRAPHING AND CALCULATIONS
+        X.append(Xf)
+        Y.append(Yf)
+        XPredFinal.append(Xf)
+        YPredFinal.append(Yf)
+        T.append(t)
+        if Yf<0:
+            Acc.append(Accuracy)
+            break
+
+#Calling Function  ////////////////////////////////////////////////////////////
+Trajectorycal(U,DelT,Xi,Yi,Verticalvel)
+print('Paylod dropped successfully','Paylod drop location',X[-1],'m','Drop time',T[-1],'sec','Accuracy =',Acc[0]*100,'%')
+
+# FOR GRAPHING PATH OF PAYLOAD 
+def Graphing(X,Y,LABEL):
+    plt.plot(X,Y,label=LABEL)
+    plt.title('Graph of Vertical Height vs Distance from the target')
+    plt.xlabel('Distance of Target in m')
+    plt.ylabel('Distance of Paylod in m')
+    plt.legend(loc='upper right')
+Graphing(X,Y,'ACTUAL PATH')
+
+## FOR CALCULATION OF HEIGHT OF ~99% ACCURACY /////////////////////////////////
+y=100
+i=0
+Ypred=0
+while True:
+    Trajectorycal(U,DelT,Xi,y,Verticalvel)
+    if Acc[i]>0.5:
+        y+=1
+        Trajectorycal(U,DelT,Xi,y,Verticalvel)
+    if Acc[i]<=0.5:
+        y-=1
+        Trajectorycal(U,DelT,Xi,y,Verticalvel)
+    if max(Acc)>0.99:
+        Ypred=y
         break
-# FOR GRAPH
-plt.plot(X,Y)
-plt.title('Graph of Vertical Height vs Distance from the target')
-plt.xlabel('Distance of Target in m')
-plt.ylabel('Distance of Paylod in m')
-plt.show()
-# /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    i+=1
+
+# FOR GRAPHING PREDICTED PATH ///////////////////////////////////////////////////
+
+XPredFinal.clear()
+YPredFinal.clear()
+X.clear()
+Y.clear()
+T.clear()
+Trajectorycal(U,DelT,Xi,Ypred,Verticalvel)
+Graphing(XPredFinal,YPredFinal,"PREDICTED PATH")
+
+# For printing the predicted path ///////////////////////////////////////////////
+
+print('Warning With Current Height only an Accuracy of',Acc[0]*100,'% can be obtained')
+print('For an accuracy of',max(Acc)*100,'% it is Reccomended to Lock on UAV to an Altitude of',Ypred,'m',',Drop Time Will be =',T[-1] ,'sec')
+
+plt.show() # It is used in the end as to plot the both  the graph on one
+
+#Created by Harsh Maurya with 💖 ............ :)🌈
